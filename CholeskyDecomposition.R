c1a0763dace2fa815697e32d3125dbbbbe193189
Halil Gorkem Herek <63462353+halilgorkem@users.noreply.github.com>
2022-10-28 12:31:51 +0300

    Cholesky Ayrışımı
    Cholesky ayrışımının Ax=b çözümleri için ek adımlar ektedir.

--- CholeskyDecomposition.R
+++ CholeskyDecomposition.R
@@ -1,8 +1,11 @@
 A <- matrix(c(4,-1,1,
               -1,4.25,2.75,
               1,2.75,3.50), ncol = 3, nrow = 3, byrow = T)
-cholesky <- function(A)
+b <- c(1,1,1)
+cholesky_decomposition <- function(A,b)
 {
+  X <- rep(0,n)
+  Y <- rep(0,n)
   n <- nrow(A)
   L <- matrix(c(0), n,n)
   
@@ -32,6 +35,25 @@ cholesky <- function(A)
   L[n,n] <- sqrt(A[n,n] - sum((L[n, 1:(n-1)])^2))
   
   #step7
-  list(L=L)
+  #list(L=L)
+  
+  #step8
+  Y <- b[1]/L[1,1]
+  
+  #step9
+  for(i in 2:n)
+  {
+    Y[i] <- (b[i] - sum(L[i,1:(i-1)]*Y[1:(i-1)]))/L[i,i]
+  }
+  #step10
+  X[n] <- Y[n]/L[n,n]
+  
+  #step11
+  for(i in (n-1):1)
+  {
+    X[i] <- (Y[i] - sum(L[(i+1):n,i]*X[(i+1):n]))/L[i,i]
+  }
+  #step12
+  list(X=X,L=L)
 }
-cholesky(A)
+cholesky_decomposition(A,b)
